c8858ad95a2fae9b87071778919d8d9b0250a2fa
LasseThomsenSkole <lath0003@stud.kea.dk>
2024-04-25 10:35:52 +0200

    innit

---
+++ src/main/java/com/example/projektkalkulation/ProjektKalkulationApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektkalkulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektKalkulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektKalkulationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projektkalkulation/ProjektKalkulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektkalkulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektKalkulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
