a0285da61c24a3ae73882856b4df94b7c09a4d64
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-05-01 11:54:14 +0200

    formatted ConnectionManager

--- src/main/java/manager/ConnectionManager.java
+++ src/main/java/manager/ConnectionManager.java
@@ -8,19 +8,18 @@ public class ConnectionManager {
     private static Connection con;
 
     private ConnectionManager() {
-        }
-
-        public static Connection getConnection(String db_url, String username, String pwd) {
-            if   (con !=   null  )
-                return con;
-            try    {
-                con =  DriverManager.getConnection(db_url, username, pwd);
-            }
-            catch (SQLException e) {
-                System.out.println("Couldn't connect to database");
-                e.printStackTrace();
-            }
-            return con;
-        }
     }
+
+    public static Connection getConnection(String db_url, String username, String pwd) {
+        if (con != null)
+            return con;
+        try {
+            con = DriverManager.getConnection(db_url, username, pwd);
+        } catch (SQLException e) {
+            System.out.println("Couldn't connect to database");
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
 
