9a78c2c57747e141e59fea608c1eb6df3337391a
AndreaRyholt <anra0003@stud.kea.dk>
2024-05-06 10:59:14 +0200

    Integration test and initial Unittest

--- src/test/java/projectmanament/ProjectTest.java
+++ src/test/java/projectmanament/ProjectTest.java
@@ -1,4 +1,30 @@
 package projectmanament;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import projectmanament.repository.ProjectRepository;
+
+import java.sql.SQLException;
+
+@SpringBootTest
+@ActiveProfiles("h2")
 public class ProjectTest {
+
+    @Autowired
+    ProjectRepository repository;
+
+    @Test
+    void testSingletonPerformance() throws SQLException {
+        for (int i = 0; i < 1000; i++) {
+            repository.getProject(1);
+        }
+    }
+
+
+
+
+
+
 }
