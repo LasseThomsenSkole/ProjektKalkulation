8d2804ff88d894e7d3d599bade52323480e19325
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-05-07 13:46:50 +0200

    refactored createProject

--- src/main/java/projectmanament/repository/ProjectRepository.java
+++ src/main/java/projectmanament/repository/ProjectRepository.java
@@ -226,7 +226,7 @@ public class ProjectRepository {
     public void createProject(String name, String description, Date deadline) {
         try {
             String SQL = "INSERT INTO projects (project_name, project_description, " +
-                    "total_hours, project_deadline) VALUES (?, ?, 0, ?);"; // sets total hours to zero
+                    "project_deadline) VALUES (?, ?, ?);";
             PreparedStatement preparedStatement = connection.prepareStatement(SQL);
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, description);
