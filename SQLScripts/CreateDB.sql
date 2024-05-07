b058883242a48cc2bcbc1eddc5c0607bc0ad2268
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-05-07 12:16:05 +0200

    CreateDB.sql is_admin not null

--- SQLScripts/CreateDB.sql
+++ SQLScripts/CreateDB.sql
@@ -3,7 +3,7 @@ USE AlphaManagement;
 CREATE TABLE IF NOT EXISTS users(
     user_id int auto_increment primary key,
     user_name varchar(50),
-    is_admin BOOL,
+    is_admin BOOL not null default false,
     password varchar(50) -- hvis den er hashed bliver den hashed med 12 chars
     );
 CREATE TABLE IF NOT EXISTS projects(
