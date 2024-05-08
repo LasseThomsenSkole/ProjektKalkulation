eb2fd29a2464c657f79be86ee6ffa4b5422c8bca
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-05-08 11:59:25 +0200

    user email attribute deleted

--- src/main/java/projectmanament/model/User.java
+++ src/main/java/projectmanament/model/User.java
@@ -3,16 +3,14 @@ package projectmanament.model;
 public class User {
     private int id;
     private String name;
-    private String email;
     private String password;
     private boolean isAdmin;
 
     public User(){} //thymeleaf
 
-    public User(int id, String name, String email, String password, boolean isAdmin) {
+    public User(int id, String name, String password, boolean isAdmin) {
         this.id = id;
         this.name = name;
-        this.email = email;
         this.password = password;
         this.isAdmin = isAdmin;
     }
